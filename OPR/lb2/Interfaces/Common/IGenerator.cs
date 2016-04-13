24435d1114ed2068337d37920a79382d016ab63f
JFF <justforfunby@gmail.com>
2016-04-13 12:09:32 +0300

    base interfeces

---
+++ OPR/lb2/Interfaces/Common/IGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OPR.lb2.Interfaces.Common
+{
+    public interface IGenerator
+    {
+        IList<T> Generate<T>(int count);
+    }
+}
---
+++ OPR/lb2/Interfaces/Common/ISeparator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OPR.lb2.Interfaces.Common
+{
+    public interface ISeparator
+    {
+        IList<T> Separate<T>(IList<T> inpuList, int count);
+    }
+}
